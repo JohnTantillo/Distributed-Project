c855dcae6e9db566414f89b827502e7da182f0fe
John Tantillo <johntantillo1@gmail.com>
2022-02-26 14:23:00 -0500

    Fixed error

--- app.py
+++ app.py
@@ -2,15 +2,13 @@
 # To run: In one terminal run "npm run build" then "npm start". Make sure you are located in the /frontend directory
 # In another terminal in the same level as the Dockerfile run "docker build -t project ." then "docker run -dp 5000:5000 project"
 # After this, navigate to localhost:5000
-#841jTjrC31zPVJLj
-#486mongo
 from flask import Flask, send_file, send_from_directory, Blueprint, request, render_template
 import pymongo
 import os
 import json
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
-client = pymongo.MongoClient("mongodb+srv://486mongo:841jTjrC31zPVJLj@cluster0.8ym8n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://486mongo:password@cluster0.8ym8n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 #client = pymongo.MongoClient('localhost')
 db = client['486db']
 col = db['games']
