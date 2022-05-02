b11e6422a7499d7e28cc10687a93e2d422d68f5e
John Tantillo <johntantillo1@gmail.com>
2022-05-02 18:44:39 -0400

    kinda working sometimes

--- Phase3_RAFT_Leader_Elections/Controller/convert_follower_node1.py
+++ Phase3_RAFT_Leader_Elections/Controller/convert_follower_node1.py
@@ -2,46 +2,107 @@ import json
 import socket
 import traceback
 import time
+import threading
 
-# Wait following seconds below sending the controller request
-time.sleep(3)
+leader = ""
 
-# Read Message Template
-msg = json.load(open("Message.json"))
+def message_handler(msg, skt):
+    request = msg['request']
+    if request == "LEADER_INFO":
+        global leader 
+        leader = msg['value']
+        msg['sender_name'] = sender
+        msg['request'] = "STORE"
+        msg['key'] = 'K1'
+        msg['value'] = 'This is the message'
+        skt.sendto(json.dumps(msg).encode(), (leader, 5555))
 
-# Initialize
-sender = "Controller"
-target = "Node1"
-port = 5555
+    elif request == "RETRIEVE":
+        print(msg['value'])
 
-# Request
-msg['sender_name'] = sender
-msg['request'] = "LEADER_INFO"
-print(f"Request Created : {msg}")
+def listener(skt):
+    print('starting listener')
+    while True:
+        try:
+            msg, addr = skt.recvfrom(1024)
+            # Decoding the Message received from leader
+            decoded_msg = json.loads(msg.decode('utf-8'))
+            # print(f"Message Received : {decoded_msg} From : {addr}")
+            threading.Thread(target=message_handler, args=[decoded_msg, skt]).start()
+        except:
+            # Timeout functionality if no messages received 
+            a = 1
 
-# Socket Creation and Binding
-skt = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-skt.bind((sender, port))
+if __name__ == "__main__":
+    # Wait following seconds below sending the controller request
+    time.sleep(5)
 
-# Send Message
-try:
-    # Encoding and sending the message
-    skt.sendto(json.dumps(msg).encode('utf-8'), (target, port))
-except:
-    #  socket.gaierror: [Errno -3] would be thrown if target IP container does not exist or exits, write your listener
-    print(f"ERROR WHILE SENDING REQUEST ACROSS : {traceback.format_exc()}")
-while True:
+    # Read Message Template
+    msg = json.load(open("Message.json"))
+    msg2 = json.load(open("Message.json"))
+    msg3 = json.load(open("Message.json"))
+    msg4 = json.load(open("Message.json"))
+    msg5 = json.load(open("Message.json"))
+
+
+    # Initialize
+    sender = "Controller"
+    target = "Node1"
+    port = 5555
+
+    # Request
+    msg['sender_name'] = sender
+    msg['request'] = "STORE"
+    msg['key'] = 'K1'
+    msg['value'] = 'This is the message'
+
+    msg2['sender_name'] = sender
+    msg2['request'] = "STORE"
+    msg2['key'] = 'K2'
+    msg2['value'] = 'This is also the message'
+
+    msg4['sender_name'] = sender
+    msg4['request'] = "STORE"
+    msg4['key'] = 'K3'
+    msg4['value'] = 'message 3'
+
+    msg5['sender_name'] = sender
+    msg5['request'] = "STORE"
+    msg5['key'] = 'K4'
+    msg5['value'] = 'message 4'
+
+
+    msg3['sender_name'] = sender
+    msg3['request'] = "RETRIEVE"
+
+    print(f"Request Created : {msg}")
+
+      # Socket Creation and Binding
+    skt = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    skt.bind((sender, port))
+
+    threading.Thread(target=listener, args=[skt]).start()
+    # Send Message
     try:
-        msg, addr = skt.recvfrom(1024)
-        decoded_msg = json.loads(msg.decode('utf-8'))
-        print(f"Message Received : {decoded_msg} From : {addr}")
-        time.sleep(1)
-        leader = decoded_msg['value']
-        # decoded_msg['request'] = 'CONVERT_FOLLOWER'
-        decoded_msg['request'] = "SHUTDOWN"
-        # decoded_msg['request'] = "TIMEOUT"
-        skt.sendto(json.dumps(decoded_msg).encode('utf-8'), (leader, port))
-        break
+        # Encoding and sending the message
+        skt.sendto(json.dumps(msg).encode('utf-8'), (target, port))
+        time.sleep(2)
+        if leader != '':
+            skt.sendto(json.dumps(msg2).encode('utf-8'), (leader, port))
+            time.sleep(2)
+            skt.sendto(json.dumps(msg4).encode('utf-8'), (leader, port))
+            time.sleep(2)
+            skt.sendto(json.dumps(msg5).encode('utf-8'), (leader, port))
+            time.sleep(2)
+            skt.sendto(json.dumps(msg3).encode('utf-8'), (leader, port))
+        else:
+            skt.sendto(json.dumps(msg2).encode('utf-8'), (leader, port))
+            time.sleep(2)
+            skt.sendto(json.dumps(msg4).encode('utf-8'), (leader, port))
+            time.sleep(2)
+            skt.sendto(json.dumps(msg5).encode('utf-8'), (leader, port))
+            time.sleep(2)
+            skt.sendto(json.dumps(msg3).encode('utf-8'), (leader, port))
     except:
+        #  socket.gaierror: [Errno -3] would be thrown if target IP container does not exist or exits, write your listener
         print(f"ERROR WHILE SENDING REQUEST ACROSS : {traceback.format_exc()}")
-        break
--- Phase3_RAFT_Leader_Elections/Node/app.py
+++ Phase3_RAFT_Leader_Elections/Node/app.py
@@ -24,6 +24,11 @@ beat_time = 0
 votes = 0
 kill = False
 current_key = 0
+nextIndex = [0,0,0,0,0]
+matchIndex = [0,0,0,0,0]
+commitIndex = 0
+lastApplied = -1
+updated = False
 
 def create_message(request_type):
     message = json.load(open("Message.json"))
@@ -32,11 +37,24 @@ def create_message(request_type):
     message['term'] = currentTerm
     message['key'] = -1
     if request_type == 'VOTE_REQUEST':
-        request_vote = {'candidateId': name, 'lastLogIndex': -1, 'lastLogTerm': -1}
-        message['value'] = request_vote
+        message['candidateId'] = name
+        message['lastLogIndex'] = -1
+        message['lastLogTerm'] = -1
     elif request_type == 'APPEND_RPC':
-        append_rpc = {'leaderId': name, 'Entries': Log, 'prevLogIndex': -1, 'prevLogTerm': -1}
-        message['value'] = append_rpc
+        message['leaderId'] = name
+        message['Entries'] = []
+        message['prevLogIndex'] = lastApplied
+        if lastApplied == -1:
+            message['prevLogTerm'] = -1
+        else:
+            message['prevLogTerm'] = Log[lastApplied]['Term']
+        message['leaderCommit'] = commitIndex
+    elif request_type == 'HEARTBEAT':
+        message['request'] = 'APPEND_RPC'
+        message['leaderId'] = name
+        message['Entries'] = []
+        message['prevLogIndex'] = -1
+        message['prevLogTerm'] = -1
     elif request_type == 'LEADER_INFO':
         message['key'] = 'LEADER'
         message['value'] = current_leader
@@ -44,6 +62,8 @@ def create_message(request_type):
         message['term'] = None
         message['key'] = 'COMMITED_LOGS'
         message['value'] = Log
+    elif request_type == 'APPEND_REPLY':
+        message['key'] = 'success'
     return message
 
 def start_election(skt):
@@ -53,6 +73,8 @@ def start_election(skt):
     global voted
     global Timeout
     global votedFor
+    global nextIndex
+    global matchIndex
     message = create_message("VOTE_REQUEST")
     for node in nodes:
         if node != name:
@@ -65,9 +87,11 @@ def start_election(skt):
         votes = 1
         votedFor = name
     currentTerm += 1
-    print('voting for myself')
+    # print('voting for myself')
     time.sleep(1)
     if state == 'candidate' and votes >= 3:
+        matchIndex = [0,0,0,0,0]
+        nextIndex = [commitIndex+1]*5
         state = 'leader'
         threading.Thread(target=heartbeat, args=[UDP_Socket]).start()
         skt.settimeout(None)
@@ -78,7 +102,7 @@ def start_election(skt):
     votes = 0
     Timeout = random.uniform(.25, .4)
     skt.settimeout(Timeout)
-    print("Ending Election")
+    # print("Ending Election")
 
 # Function to decode different message types
 def message_handler(msg, skt):
@@ -88,10 +112,16 @@ def message_handler(msg, skt):
     global state
     global current_leader
     global kill
+    global lastApplied
+    global commitIndex
+    global Log
+    global nextIndex
+    global matchIndex
+    global updated
     new_time = time.perf_counter()
     if new_time - beat_time > Timeout and state == 'follower':
         state = 'candidate'
-        print("Starting Election")
+        # print("Starting Election")
         threading.Thread(target=start_election, args=[skt]).start()
     
     request = msg['request']
@@ -107,13 +137,60 @@ def message_handler(msg, skt):
             voted = False
             beat_time = time.perf_counter()
             current_leader = msg['leaderId']
-            print('Heartbeat from ' + msg['leaderId'])
+            # print('Heartbeat from ' + msg['leaderId'])
+        else:
+            # if msg['prevLogIndex'] > -1:
+            #     print(Log[msg['prevLogIndex']])
+            if msg['prevLogIndex'] == -1:
+                Log.append(msg['Entries'][0])
+                lastApplied += 1
+                print(Log)
+                print(lastApplied)
+                reply = create_message('APPEND_REPLY')
+                reply['value'] = True
+                skt.sendto(json.dumps(reply).encode(), (current_leader, 5555)) # CHANGE RETURNS TO SEND APPEND REPLY
+
+            elif msg['prevLogIndex'] >= len(Log):
+                print("index too long")
+                print(len(Log))
+                print(msg['prevLogIndex'])
+                reply = create_message('APPEND_REPLY')
+                reply['value'] = False
+                skt.sendto(json.dumps(reply).encode(), (current_leader, 5555))
+
+            elif msg['term'] < currentTerm or Log[msg['prevLogIndex']]['Term'] != msg['prevLogTerm']:
+                print('Something isnt matching')
+                print('message: ' + str(msg))
+                print('Log: ' + str(Log))
+                print('current term: ' + str(currentTerm))
+                reply = create_message('APPEND_REPLY')
+                reply['value'] = False
+                skt.sendto(json.dumps(reply).encode(), (current_leader, 5555))
+                
+            elif msg['prevLogIndex'] + 1 < len(Log):
+                if Log[msg['prevLogIndex']+1]['Term'] != msg['term']:
+                    Log = Log[:msg['prevLogIndex']+1]
+                reply = create_message('APPEND_REPLY')
+                reply['value'] = True
+                skt.sendto(json.dumps(reply).encode(), (current_leader, 5555))
+
+            else:
+                Log.append(msg['Entries'][0])
+                lastApplied += 1
+                print(Log)
+                print(lastApplied)
+                reply = create_message('APPEND_REPLY')
+                reply['value'] = True
+                skt.sendto(json.dumps(reply).encode(), (current_leader, 5555))
+
+            if msg['leaderCommit'] > commitIndex:
+                commitIndex = msg['leaderCommit']
 
     elif request == 'VOTE_REQUEST':
-        if currentTerm <= msg['Term'] and not voted:
+        if currentTerm <= msg['term'] and not voted:
             voted = True
             node = msg['candidateId']
-            print("voting for " + node)
+            # print("voting for " + node)
             vote_reply = create_message('VOTE_ACK')
             skt.sendto(json.dumps(vote_reply).encode(), (node, 5555))
             global votedFor
@@ -143,11 +220,12 @@ def message_handler(msg, skt):
         reply = create_message("LEADER_INFO")
         skt.sendto(json.dumps(reply).encode(), ('Controller', 5555))
         print('The current leader is ' + current_leader)
-        return {'LEADER':current_leader}
 
     elif request == "STORE":
         if state == 'leader':
-            Log.append({'Term': msg['term'], 'Key': msg['key'], 'Value': msg['value']})
+            entry = {'Term': currentTerm, 'Key': msg['key'], 'Value': msg['value']}
+            Log.append(entry)
+            updated = True
         else:
             reply = create_message("LEADER_INFO")
             skt.sendto(json.dumps(reply).encode(), ('Controller', 5555))
@@ -157,7 +235,35 @@ def message_handler(msg, skt):
             reply = create_message("RETRIEVE")
         else:
             reply = create_message("LEADER_INFO")
-            skt.sendto(json.dumps(reply).encode(), ('Controller', 5555))
+        skt.sendto(json.dumps(reply).encode(), ('Controller', 5555))
+   
+    elif request == 'APPEND_REPLY':
+        node_num = int(msg['sender_name'][-1])-1
+        if msg['value'] == True:
+            matchIndex[node_num] += 1
+            nextIndex[node_num] += 1
+        else: 
+            if nextIndex[node_num] != 0:
+                nextIndex[node_num] -= 1
+                app_rpc = create_message("APPEND_RPC")
+                app_rpc['prevLogIndex'] = nextIndex[node_num]
+                app_rpc['prevLogTerm'] = Log[nextIndex[node_num]]['Term']
+                app_rpc['Entries'].append(Log[nextIndex[node_num]])
+                skt.sendto(json.dumps(app_rpc).encode(), (msg['sender_name'], 5555))
+        matcher = {}
+        for i in matchIndex:
+            if i not in matcher:
+                matcher[i] = 1
+            else:
+                for key in matcher:
+                    if i >= matcher[key]:
+                        matcher[key] += 1
+        for ind in matcher:
+            if matcher[ind] >= 2 and ind > commitIndex:
+                commitIndex = ind
+        print("commit index: " + str(commitIndex))
+        print('match index: ' + str(matchIndex))
+        print('next index: ' + str(nextIndex))
     else:
         print("UNSUPPORTED MESSAGE TYPE, PLEASE RECONSIDER")
             
@@ -191,19 +297,35 @@ def listener(skt):
     print("Exiting Listener Function")
 
 def heartbeat(skt):
-    beat = create_message('APPEND_RPC')
+    global updated
+    global lastApplied
+    print('I am the leader')
+    beat = create_message('HEARTBEAT')
     while True:
         data = {"currentTerm": currentTerm, "votedFor": votedFor, "Log": [], "Timeout": Timeout, 'Heartbeat': Heartbeat}
         if kill:
             return
         if state == 'leader':
             time.sleep(Heartbeat)
-            for node in nodes:
-                if node != name:
-                    try:
-                        skt.sendto(json.dumps(beat).encode(), (node, 5555))
-                    except:
-                        print(node + ' is down')
+            if not updated:
+                for node in nodes:
+                    if node != name:
+                        try:
+                            skt.sendto(json.dumps(beat).encode(), (node, 5555))
+                        except:
+                            print(node + ' is down')
+            else:
+                updated = False
+                app_rpc = create_message("APPEND_RPC")
+                lastApplied += 1
+                app_rpc['Entries'].append(Log[lastApplied])
+                for node in nodes:
+                    if node != name:
+                        try:
+                            skt.sendto(json.dumps(app_rpc).encode(), (node, 5555))
+                        except:
+                            print(node + ' is down')
+                print(Log)
             with open(name+".txt", 'w') as f:
                 f.write(str(data))
                     # print("sent to " + node)
@@ -211,13 +333,13 @@ def heartbeat(skt):
 if __name__ == "__main__":
     with open(name+".txt", 'r') as f:
         info = f.read()
-        print(info)
-    global Timeout
-    global currentTerm
-    global votedFor
-    Timeout = info['Timeout']
-    currentTerm = info['currentTerm']
-    votedFor = info['votedFor']
+        # print(info)
+    # global Timeout
+    # global currentTerm
+    # global votedFor
+    # Timeout = info['Timeout']
+    # currentTerm = info['currentTerm']
+    # votedFor = info['votedFor']
     # Creating Socket and binding it to the target container IP and port
     UDP_Socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
